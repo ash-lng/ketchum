fa86ef3c996e6b176e3f14c60c478f2806a1a9d2
Geoffrey Hayes <hayesgm@gmail.com>
2022-11-17 19:18:49 -0800

    Ash Core Mod Test

--- src/AshCoreMod/Cool.js
+++ src/AshCoreMod/Cool.js
@@ -1,7 +1,9 @@
-var _Cool_alert = F1(function(msg) {
-  alert("msg " + msg);
-  return {
-    $: 0,
-    __type: msg,
-  };
-});
+
+function _Cool_alert(message)
+{
+  return A2(__Task_perform, __Basics_never, __Scheduler_binding(function(callback)
+  {
+    alert("msg " + message);
+  }));
+}
+
--- src/Main.elm
+++ src/Main.elm
@@ -1,23 +1,39 @@
-module Main exposing (main)
+module Main exposing (main, update)
 
+import Browser
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+
 import Cool exposing (alert)
 
+type Msg = Alert String
 
-view : () -> Html msg
+update : Msg -> () -> ( (), Cmd Msg )
+update msg () =
+    case msg of
+        Alert message ->
+            ((), alert message)
+
+view : () -> Html Msg
 view () =
     div [ class "jumbotron" ]
         [ h1 [] [ text "Welcome Ash Ketchum!" ]
-        , p [ onClick (alert "Cool" )]
+        , p [ onClick (Alert "Cool" )]
             [ text <|
                 """ 
                 You and Pikachu finally made it! 🥇
                 """
-            , text (print (String.fromInt (1 + 2)))
+            , text (String.fromInt (1 + 2))
             ]
         ]
 
 
+main : Program () () Msg
 main =
-    view ()
+    Browser.element
+        { view = view
+        , init = \() -> ((), Cmd.none)
+        , update = update
+        , subscriptions = (\model -> Sub.none)
+        }
